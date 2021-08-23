04e537a9a9d1417c11835462762456521fd87fc9
Nikolay Penkov <nikolay.penkov@iosb.fraunhofer.de>
2021-08-23 11:13:47 +0200

    Update

--- nginx/create_keys.sh
+++ nginx/create_keys.sh
@@ -17,10 +17,13 @@ read cert_validity
 echo "Enter a password for the keystore: (min 6 characters)"
 read -s key_store_pass
 
+echo "(Optional) Enter additional dns names (or IPs) for which the certificate should be valid. (Example: IP:127.0.0.1,DNS:my.custom.dns,DNS:my2.custom.dns,IP:123.123.123.123)"
+read custom_addresses
+
 # Creating cert for custom address
 mkdir ssl
 
-keytool -genkeypair -keyalg RSA -keysize 2048 -alias SympClient -dname "CN=$server_address,OU=$server_ou,O=$server_o,C=$server_c" -validity $cert_validity -keystore ./ssl/keystore.jks -storepass $key_store_pass -keypass $key_store_pass -deststoretype pkcs12
+keytool -genkeypair -keyalg RSA -keysize 2048 -alias SympIngress -dname "CN=$server_address,OU=$server_ou,O=$server_o,C=$server_c" -ext "SAN=DNS:$server_address,$custom_addresses" -validity $cert_validity -keystore ./ssl/keystore.jks -storepass $key_store_pass -keypass $key_store_pass -deststoretype pkcs12
 
 keytool -importkeystore -srckeystore ./ssl/keystore.jks -srcstorepass $key_store_pass -destkeystore ./ssl/client.p12 -deststorepass $key_store_pass -deststoretype pkcs12
 
@@ -30,7 +33,7 @@ openssl pkcs12 -in ./ssl/client.p12 -out ./ssl/cert.crt -nokeys -password pass:$
 #Creating wildcard cert for address *service-name*.ingress.docker
 mkdir ssl_wildcard
 
-keytool -genkeypair -keyalg RSA -keysize 2048 -alias SympClient -dname "CN=*.ingress.docker,OU=$server_ou,O=$server_o,C=$server_c" -validity $cert_validity -keystore ./ssl_wildcard/keystore.jks -storepass $key_store_pass -keypass $key_store_pass -deststoretype pkcs12
+keytool -genkeypair -keyalg RSA -keysize 2048 -alias SympIngress -dname "CN=*.ingress.docker,OU=$server_ou,O=$server_o,C=$server_c" "SAN=DNS:$server_address,$custom_addresses" -validity $cert_validity -keystore ./ssl_wildcard/keystore.jks -storepass $key_store_pass -keypass $key_store_pass -deststoretype pkcs12
 
 keytool -importkeystore -srckeystore ./ssl_wildcard/keystore.jks -srcstorepass $key_store_pass -destkeystore ./ssl_wildcard/client.p12 -deststorepass $key_store_pass -deststoretype pkcs12
 
