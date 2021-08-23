b306ba5299b78fb3420cf886c13f0cbdeccd0a73
Nikolay Penkov <nikolay.penkov@iosb.fraunhofer.de>
2021-08-23 11:14:45 +0200

    Update

--- nginx/create_keys.sh
+++ nginx/create_keys.sh
@@ -25,20 +25,20 @@ mkdir ssl
 
 keytool -genkeypair -keyalg RSA -keysize 2048 -alias SympIngress -dname "CN=$server_address,OU=$server_ou,O=$server_o,C=$server_c" -ext "SAN=DNS:$server_address,$custom_addresses" -validity $cert_validity -keystore ./ssl/keystore.jks -storepass $key_store_pass -keypass $key_store_pass -deststoretype pkcs12
 
-keytool -importkeystore -srckeystore ./ssl/keystore.jks -srcstorepass $key_store_pass -destkeystore ./ssl/client.p12 -deststorepass $key_store_pass -deststoretype pkcs12
+keytool -importkeystore -srckeystore ./ssl/keystore.jks -srcstorepass $key_store_pass -destkeystore ./ssl/ingress.p12 -deststorepass $key_store_pass -deststoretype pkcs12
 
-openssl pkcs12 -in ./ssl/client.p12 -nodes -nocerts -out ./ssl/private.pem -password pass:$key_store_pass
-openssl pkcs12 -in ./ssl/client.p12 -out ./ssl/cert.crt -nokeys -password pass:$key_store_pass
+openssl pkcs12 -in ./ssl/ingress.p12 -nodes -nocerts -out ./ssl/private.pem -password pass:$key_store_pass
+openssl pkcs12 -in ./ssl/ingress.p12 -out ./ssl/cert.crt -nokeys -password pass:$key_store_pass
 
 #Creating wildcard cert for address *service-name*.ingress.docker
 mkdir ssl_wildcard
 
 keytool -genkeypair -keyalg RSA -keysize 2048 -alias SympIngress -dname "CN=*.ingress.docker,OU=$server_ou,O=$server_o,C=$server_c" "SAN=DNS:$server_address,$custom_addresses" -validity $cert_validity -keystore ./ssl_wildcard/keystore.jks -storepass $key_store_pass -keypass $key_store_pass -deststoretype pkcs12
 
-keytool -importkeystore -srckeystore ./ssl_wildcard/keystore.jks -srcstorepass $key_store_pass -destkeystore ./ssl_wildcard/client.p12 -deststorepass $key_store_pass -deststoretype pkcs12
+keytool -importkeystore -srckeystore ./ssl_wildcard/keystore.jks -srcstorepass $key_store_pass -destkeystore ./ssl_wildcard/ingress.p12 -deststorepass $key_store_pass -deststoretype pkcs12
 
-openssl pkcs12 -in ./ssl_wildcard/client.p12 -nodes -nocerts -out ./ssl_wildcard/private.pem -password pass:$key_store_pass
-openssl pkcs12 -in ./ssl_wildcard/client.p12 -out ./ssl_wildcard/cert.crt -nokeys -password pass:$key_store_pass
+openssl pkcs12 -in ./ssl_wildcard/ingress.p12 -nodes -nocerts -out ./ssl_wildcard/private.pem -password pass:$key_store_pass
+openssl pkcs12 -in ./ssl_wildcard/ingress.p12 -out ./ssl_wildcard/cert.crt -nokeys -password pass:$key_store_pass
 
 echo Done
 
